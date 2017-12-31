eeffb49f2927d493f1e4bf1daaafacdf3e29f641
wj3ng <willie.jeng@gmail.com>
2017-12-31 17:26:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.on("ready", function(){
 });
 
 bot.on("message",function(message){
-    if(message.includes("(╯°□°）╯︵ ┻━┻")) message.channel.send("┬─┬ ノ( ゜-゜ノ)   請尊重桌子");
+    if(message.content.includes("(╯°□°）╯︵ ┻━┻")) message.channel.send("┬─┬ ノ( ゜-゜ノ)   請尊重桌子");
 
     if(message.author.equals(bot.user)) return;
     if(!message.content.startsWith(PREFIX)) return;
