ff3c287a60bc65e303c01a5077bd6467f8067f82
wj3ng <willie.jeng@gmail.com>
2017-12-31 16:43:38 +0800

    Turned reply into channel.send, removed default case

--- index.js
+++ index.js
@@ -19,20 +19,20 @@ bot.on("message",function(message){
 
     switch(args[0].toLowerCase()){
         case "help":
-            message.reply("OJ Linker 的指令都是 `~` 開頭的.\n`~zj ProblemID` : ZeroJudge 的題目連結 (`ProblemID`用題號取代)\n`~tioj ProblemID` : TIOJ 的題目連結 (`ProblemID`用題號取代)\n`~neoj ProblemID` : NEOJ 的題目連結 (`ProblemID`用題號取代)\n`~誰是世界上最醜的人` : 宣布世界上最醜的人");
+            message.channel.send("OJ Linker 的指令都是 `~` 開頭的.\n`~zj ProblemID` : ZeroJudge 的題目連結 (`ProblemID`用題號取代)\n`~tioj ProblemID` : TIOJ 的題目連結 (`ProblemID`用題號取代)\n`~neoj ProblemID` : NEOJ 的題目連結 (`ProblemID`用題號取代)\n`~誰是世界上最醜的人` : 宣布世界上最醜的人");
             break;
         case "zj":
-            if(!args[1]) message.reply('請加上題號 (正確範例: "~zj a001")');
-            else if(args[1].length != 4) message.reply('題號格式不正確 (正確範例: "~zj a001")');
+            if(!args[1]) message.channel.send('請加上題號 (正確範例: "~zj a001")');
+            else if(args[1].length != 4) message.channel.send('題號格式不正確 (正確範例: "~zj a001")');
             else message.channel.send("Zerojudge " + args[1] + ": https://zerojudge.tw/ShowProblem?problemid=" + args[1].toLowerCase());
             break;
         case "tioj":
-            if(!args[1]) message.reply('請加上題號 (正確範例: "~tioj 1001")');
-            else if(args[1].length != 4) message.reply('題號格式不正確 (正確範例: "~tioj 1001")');
+            if(!args[1]) message.channel.send('請加上題號 (正確範例: "~tioj 1001")');
+            else if(args[1].length != 4) message.channel.send('題號格式不正確 (正確範例: "~tioj 1001")');
             else message.channel.send("TIOJ " + args[1] + ": http://tioj.ck.tp.edu.tw/problems/" + args[1]);
             break;
         case "neoj":
-            if(!args[1]) message.reply('請加上題號 (正確範例: "~neoj 1")');
+            if(!args[1]) message.channel.send('請加上題號 (正確範例: "~neoj 1")');
             else message.channel.send("NEOJ " + args[1] + ": https://neoj.sprout.tw/problem/" + args[1]);
             break;
         case "誰是世界上最醜的人":
@@ -42,8 +42,6 @@ bot.on("message",function(message){
                 .setImage(message.author.avatarURL);
             message.channel.send(embed);
             break;
-        default:
-            message.reply("Invalid command. Type ~help for a list of commands.")
     }
 
 });
