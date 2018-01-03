db89e833640b96b1bb5e35058c87c5d7d39f4c5e
Willie Jeng <willie.jeng@gmail.com>
2018-01-03 20:44:18 +0800

    testing modules

---
+++ ojlinker.js
@@ -0,0 +1,46 @@
+var isNum = function(c){
+	for(i=0; i<10; i++)
+		if(c == i) return true;
+	return false;
+}
+
+var zjValID = function(id){
+	if(id.length != 4) return false;
+	if(id.charAt(0)!='a' && id.charAt(0)!='b' && id.charAt(0)!='c' && id.charAt(0)!='d') return false;
+	for(i=1; i<4; i++)
+		if(!isNum(id.charAt(i))) return false;
+	return true;
+}
+
+var tiojValID = function(id){
+	if(id.length != 4) return false;
+	for(i=0; i<4; i++)
+		if(!isNum(id.charAt(i))) return false;
+	return true;
+}
+
+var neojValID = function(id){
+	if(id.length > 4) return false;
+	for(i=0; i<id.length; i++)
+		if(!isNum(id.charAt(i))) return false;
+	return true;
+}
+
+module.exports = {
+	zj: function(id){
+		if(!id) return '請加上題號 (正確範例: "~zj a001")';
+		if(!zjValID(id)) return '題號格式不正確 (正確範例: "~zj a001")';
+		return "ZeroJudge ${id}: http://zerojudge.tw/ShowProblem?problemid=${id}";
+	},
+	tioj: function(id){
+		if(!id) return '請加上題號 (正確範例: "~tioj 1001")';
+		if(!tiojValID(id)) return '題號格式不正確 (正確範例: "~tioj 1001")';
+		return "TIOJ ${id}: http://tioj.ck.tp.edu.tw/problems/${id}";
+	},
+	neoj: function(id){
+		if(!id) return '請加上題號 (正確範例: "~neoj 1")';
+		if(!neojValID(id)) return '題號格式不正確 (正確範例: "~neoj 1")';
+		return "NEOJ ${id}: https://neoj.sprout.tw/problem/${id}";
+	}
+}
+
---
+++ second.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const Linker = require('./ojlinker')
+
+var client = new Discord.client();
+
+var PREFIX = "~";
+
+client.on("message", message => {
+	if(message.author.bot) return;
+
+	//....
+
+	if(!message.content.startsWith(PREFIX)) return;
+	var args = message.content.substring(PREFIX.length).split(" ");
+
+	switch(args[0].toLowerCase()){
+		case "help":
+			message.channel.send(
+				":cancer: MasterBot69 :cancer: 的指令都是 `~` 開頭的.\n" + 
+				"`~zj ProblemID` : ZeroJudge 的題目連結 (`ProblemID`用題號取代)\n" + 
+				"`~tioj ProblemID` : TIOJ 的題目連結 (`ProblemID`用題號取代)\n" +
+				"`~neoj ProblemID` : NEOJ 的題目連結 (`ProblemID`用題號取代)\n\n" + 
+				"我是:cancer:餌神:cancer:的傑作!\n" + 
+				"我的原始碼: https://github.com/wj3ng/oj-linker"
+			);
+		case "zj":
+			message.channel.send(Linker.zj(args[1]));
+			break;
+		case "tioj":
+			message.channel.send(Linker.tioj(args[1]));
+			break;
+		case "neoj":
+			message.channel.send(Linker.neoj(args[1]));
+			break;
+	}
+
+
+});
+
+client.on("ready", () => {
+	console.log("MasterBot69 is ready.")
+
+	bot.user.setPresence({
+		game:{
+			name : PREFIX + "help",
+			type : 0
+		}
+	});
+});
