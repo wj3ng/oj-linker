877d03a762da42c69dda8e4fe2888e9235e391e3
Willie Jeng <willie.jeng@gmail.com>
2018-01-08 11:20:47 +0800

    i put the body in the wrong place (that sounds weird)

--- contestquery.js
+++ contestquery.js
@@ -7,7 +7,7 @@ module.exports = {
 	cfQuery: function(channel){
 		console.log("CF QUERY");
 		console.log(body[i].name);
-		request("http://codeforces.com/api/contest.list?gym=false", {json: true}, function(err,body,resp){
+		request("http://codeforces.com/api/contest.list?gym=false", {json: true}, function(err, resp, body){
 			for(i=0; body[i].phase=="BEFORE"; i++){
 				if(body[i].relativeTimeSeconds<1800 && !announced.contains(body[i].id)){
 					announced.push(body[i].id);
