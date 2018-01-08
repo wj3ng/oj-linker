15fab5e6e58391241ec00e5c64513abf0b2af929
Willie Jeng <willie.jeng@gmail.com>
2018-01-08 11:32:14 +0800

    it's almost working

--- contestquery.js
+++ contestquery.js
@@ -10,7 +10,7 @@ module.exports = {
 			console.log(body.result[0].name);
 
 			for(i=0; body.result[i].phase=="BEFORE"; i++){
-				if(body.result[i].relativeTimeSeconds<1800 && !announced.contains(body[i].id)){
+				if(body.result[i].relativeTimeSeconds<1800 && !announced.contains(body.result[i].id)){
 					announced.push(body.result[i].id);
 					channel.send("30分鐘後有 CodeForces 競賽!");
 					var cfEmbed = new Discord.RichEmbed()
