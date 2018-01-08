dff6f4c723049edfc81d8e9d0ac2e47e2615e511
Willie Jeng <willie.jeng@gmail.com>
2018-01-08 11:30:23 +0800

    turns out i'm json illiterate

--- contestquery.js
+++ contestquery.js
@@ -7,14 +7,15 @@ module.exports = {
 	cfQuery: function(channel){
 		console.log("CF QUERY");
 		request("http://codeforces.com/api/contest.list?gym=false", {json: true}, function(err, resp, body){
-			console.log(body[0].name);
-			for(i=0; body[i].phase=="BEFORE"; i++){
-				if(body[i].relativeTimeSeconds<1800 && !announced.contains(body[i].id)){
-					announced.push(body[i].id);
+			console.log(body.result[0].name);
+
+			for(i=0; body.result[i].phase=="BEFORE"; i++){
+				if(body.result[i].relativeTimeSeconds<1800 && !announced.contains(body[i].id)){
+					announced.push(body.result[i].id);
 					channel.send("30分鐘後有 CodeForces 競賽!");
 					var cfEmbed = new Discord.RichEmbed()
-						.setField(body[i].name,"Duration: " + body[i].durationSeconds/60 +
-							" 分鍾\nhttp://codeforces.com/contest/" + body[i].id);
+						.setField(body.result[i].name,"Duration: " + body.result[i].durationSeconds/60 +
+							" 分鍾\nhttp://codeforces.com/contest/" + body.result[i].id);
 						channel.send(cfEmbed);
 						//set color
 				}
